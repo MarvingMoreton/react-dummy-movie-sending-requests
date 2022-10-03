7f890c4bced6a7a04dcc5902e40e84fdc2578ca7
Marving Moreton <moreton.marving@gmail.com>
2022-10-03 17:11:19 -0400

    sending https request within React app to REST API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    // Using a GET method (per default)
+    // Pass as a string the URL we want to fetch
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        //the response is an object with a bunch of DataTransfer.
+        return response.json();
+        // response.json() return a promise
+      })
+      .then((data) => {
+        // Transforming the data results here because of underscore key pair _
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
